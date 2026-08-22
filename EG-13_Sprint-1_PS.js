fb6aa5128847fe563e39340c4e5a675a08e32194
Anjumanara Yeasmeen <malarashid14@gmail.com>
2026-08-22 22:51:29 +0600

    fibonacci solutions

--- EG-13_Sprint-1_PS.js
+++ EG-13_Sprint-1_PS.js
@@ -4,7 +4,7 @@
  */
 function isLeapYear(year) {
     // Write your code here
-  function isLeapYear(year) {
+
     if (year % 400 === 0) {
         return true;
     } else if (year % 100 === 0) {
@@ -15,4 +15,29 @@ function isLeapYear(year) {
         return false;
     }
 }
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+function generateFibonacci(n) {
+    let result = [];
+
+    if (n <= 0) {
+        return result;
+    }
+
+    if (n >= 1) {
+        result.push(0);
+    }
+
+    if (n >= 2) {
+        result.push(1);
+    }
+
+    for (let i = 2; i < n; i++) {
+        result.push(result[i - 1] + result[i - 2]);
+    }
+
+    return result;
 }
